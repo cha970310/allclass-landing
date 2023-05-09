0a8afd3a80ccba9ddc6940bd36b4fd56d7b408f2
cha970310 <cha970310@gmail.com>
2023-05-09 10:42:46 +0900

    bg color changed

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({children}){
 
     return(
         
-        <div className="bg-primary">
+        <div className="bg-white">
         
         <Header></Header>
         <div>{children}</div>
